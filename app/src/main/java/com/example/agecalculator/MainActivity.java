83af246196c46e8ad093836e2ae7e768e24158da
N Mitash <63482714+nilaymitash@users.noreply.github.com>
2023-03-23 17:41:27 -0400

    initial commit

---
+++ app/src/main/java/com/example/agecalculator/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.agecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mFirstNameInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFirstNameInput = findViewById(R.id.first_name_input);
+        //mFirstNameInput.setText("Testing nested layout");
+    }
+}
